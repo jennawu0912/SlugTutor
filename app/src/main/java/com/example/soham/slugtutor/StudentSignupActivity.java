991b3acd742731d66332b70029eee17691205bc3
Soham Saha <sohamjs2@gmail.com>
2017-04-23 11:25:07 -0700

    Initial commit

---
+++ app/src/main/java/com/example/soham/slugtutor/StudentSignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.soham.slugtutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_signup);
+    }
+}
