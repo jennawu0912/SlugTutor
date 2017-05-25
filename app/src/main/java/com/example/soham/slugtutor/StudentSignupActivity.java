ca67e9664825bb33ac07b7badda5d211af9a51be
Soham Saha <sohamjs2@gmail.com>
2017-05-25 00:52:28 -0700

    Added signup functionality

--- app/src/main/java/com/example/soham/slugtutor/StudentSignupActivity.java
+++ app/src/main/java/com/example/soham/slugtutor/StudentSignupActivity.java
@@ -1,7 +1,20 @@
 package com.example.soham.slugtutor;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StudentSignupActivity extends AppCompatActivity {
 
@@ -9,5 +22,55 @@ public class StudentSignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_signup);
+
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        final EditText emailText = (EditText) findViewById(R.id.editEmail);
+        final EditText passwordText = (EditText) findViewById(R.id.editPassword);
+        Button btnSignUp = (Button) findViewById(R.id.submitButton);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = emailText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                String ucscEmail = email.substring(email.indexOf("@")+1);
+
+                if (!ucscEmail.equals("ucsc.edu")){
+                    Toast.makeText(getApplicationContext(), "UCSC email only!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(StudentSignupActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(StudentSignupActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(StudentSignupActivity.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(StudentSignupActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
     }
+
 }
