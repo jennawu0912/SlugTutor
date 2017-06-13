e0f013002fb7c2be1372deae2c965301de801e9a
Jenna Wu <jennawu0912@gmail.com>
2017-06-13 04:25:02 -0700

    displays all users on main page but not clickable and only users names

--- app/src/main/java/com/example/soham/slugtutor/Main_Area/Search/SearchActivity.java
+++ app/src/main/java/com/example/soham/slugtutor/Main_Area/Search/SearchActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +19,11 @@ import android.widget.TextView;
 
 import com.example.soham.slugtutor.Main_Area.NavigationHelper;
 import com.example.soham.slugtutor.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.util.ArrayList;
@@ -28,6 +34,11 @@ import java.util.List;
  */
 
 public class SearchActivity extends AppCompatActivity{
+
+    private DatabaseReference mDatabaseRef;
+    final String USERGROUP = "JennasUsers";
+
+    private ValueEventListener mPostListener;
 
     private class MyAdapter extends ArrayAdapter<ListElement> {
 
@@ -76,6 +87,7 @@ public class SearchActivity extends AppCompatActivity{
     private static final int ACTIVITY_NUM = 1;
     private Context mContext = SearchActivity.this;
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -86,16 +98,60 @@ public class SearchActivity extends AppCompatActivity{
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, classes);
         dropdown.setAdapter(myAdapter);
 
-        ArrayList<ListElement> entryList = new ArrayList<ListElement>();
-        for(int i = 0; i < 20; i++) {
-            ListElement in = new ListElement("Dustin");
-            entryList.add(in);
-        }
 
-        MyAdapter adapter = new MyAdapter(this, R.layout.information_layout, entryList);
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference().child(USERGROUP);
+        final List<String> info = new ArrayList<String>();
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (DataSnapshot msgSnapShot: dataSnapshot.getChildren()) {
+                    String temp = msgSnapShot.child("firstname").getValue(String.class);
+                    //Log.d("name: ", temp);
+                    info.add(temp);
+                }
+
+                ArrayList<ListElement> entryList = new ArrayList<ListElement>();
+                for(int i = 0; i < info.size(); i++) {
+                    ListElement in = new ListElement(info.get(i));
+                    entryList.add(in);
+                    Log.d("test: ", info.get(i));
+                }
+
+
+                MyAdapter adapter = new MyAdapter(SearchActivity.this, R.layout.information_layout, entryList);
+                ListView listView = (ListView) findViewById(R.id.list_view);
+                listView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // [START_EXCLUDE]
+                //Toast.makeText(PostDetailActivity.this, "Failed to load post.",
+                //Toast.LENGTH_SHORT).show();
+                // [END_EXCLUDE]
+            }
+        };
+        mDatabaseRef.addValueEventListener(postListener);
+        // [END post_value_event_listener]
+
+        // Keep copy of post listener so we can remove it when app stops
+        mPostListener = postListener;
+
+
+        Log.d("test: ", Integer.toString(info.size()));
+
+
+
+
+
     }
+
+
 
     private void setupBottomNavigationView(){
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx)findViewById(R.id.NavBot);
