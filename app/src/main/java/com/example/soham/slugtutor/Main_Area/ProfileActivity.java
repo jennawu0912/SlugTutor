ba40b090e184ae92fbc017f411d1b2881c8016e4
David_Trang <datrang@ucsc.edu>
2017-06-14 11:54:35 -0700

    Final code

--- app/src/main/java/com/example/soham/slugtutor/Main_Area/ProfileActivity.java
+++ app/src/main/java/com/example/soham/slugtutor/Main_Area/ProfileActivity.java
@@ -9,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.soham.slugtutor.R;
@@ -20,6 +22,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +38,7 @@ public class ProfileActivity extends AppCompatActivity{
     private Context mContext = ProfileActivity.this;
     FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
     DatabaseReference mDatabaseRef = mDatabase.getReference();
+    DatabaseReference mDatabaseRef2 = mDatabase.getReference();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -43,16 +48,124 @@ public class ProfileActivity extends AppCompatActivity{
         setupBottomNavigationView();
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = new String();
+        final String uid = new String();
+        String uid2=uid;
 
         if (user != null) {
-            uid = user.getUid();
+            uid2 = user.getUid();
         }else {
             Log.d("error : ", "user is null");
         }
 
         // Initialize Database
         mDatabaseRef = mDatabaseRef.child("users").child(uid);
+        mDatabaseRef2 = mDatabaseRef2.child("users").child(uid2);
+
+        final EditText user_firstname = (EditText) findViewById(R.id.display_name);
+        final EditText user_lastname = (EditText) findViewById(R.id.display_lastname);
+        final EditText user_major = (EditText) findViewById(R.id.display_major);
+        final EditText user_phone = (EditText) findViewById(R.id.display_phone);
+        final EditText user_course = (EditText) findViewById(R.id.display_course);
+        final EditText user_status = (EditText) findViewById(R.id.display_status);
+        final EditText user_email = (EditText) findViewById(R.id.display_email);
+
+
+
+        user_firstname.setEnabled(false);
+        user_firstname.setClickable(false);
+
+        user_lastname.setEnabled(false);
+        user_lastname.setClickable(false);
+
+        user_major.setEnabled(false);
+        user_major.setClickable(false);
+
+        user_phone.setEnabled(false);
+        user_phone.setClickable(false);
+
+        user_course.setEnabled(false);
+        user_course.setClickable(false);
+
+        user_status.setEnabled(false);
+        user_status.setClickable(false);
+
+        user_email.setEnabled(false);
+        user_email.setClickable(false);
+
+        ImageButton editProfile = (ImageButton)  findViewById(R.id.editProfile);
+        final Button submitButton = (Button) findViewById(R.id.submit);
+        submitButton.setVisibility(View.GONE);
+
+        editProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+
+                submitButton.setVisibility(View.VISIBLE);
+
+                user_firstname.setEnabled(true);
+                user_firstname.setClickable(true);
+
+                user_lastname.setEnabled(true);
+                user_lastname.setClickable(true);
+
+                user_major.setEnabled(true);
+                user_major.setClickable(true);
+
+                user_phone.setEnabled(true);
+                user_phone.setClickable(true);
+
+                user_course.setEnabled(true);
+                user_course.setClickable(true);
+
+                user_email.setEnabled(true);
+                user_email.setClickable(true);
+
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+              submitButton.setVisibility(View.GONE);
+
+                user_firstname.setEnabled(false);
+                user_firstname.setClickable(false);
+
+                user_lastname.setEnabled(false);
+                user_lastname.setClickable(false);
+
+                user_major.setEnabled(false);
+                user_major.setClickable(false);
+
+                user_phone.setEnabled(false);
+                user_phone.setClickable(false);
+
+                user_course.setEnabled(false);
+                user_course.setClickable(false);
+
+                user_email.setEnabled(false);
+                user_email.setClickable(false);
+
+                String first = user_firstname.getText().toString();
+                String last = user_lastname.getText().toString();
+                String major = user_major.getText().toString();
+                String course = user_course.getText().toString();
+                String phone = user_phone.getText().toString();
+
+                mDatabaseRef2.child(uid).child("firstname").setValue(first);
+                mDatabaseRef2.child(uid).child("lastname").setValue(last);
+                mDatabaseRef2.child(uid).child("phonenumber").setValue(phone);
+                mDatabaseRef2.child(uid).child("major").setValue(major);
+                mDatabaseRef2.child(uid).child("course").setValue(course);
+
+                finish();
+                overridePendingTransition(0,0);
+                startActivity(getIntent());
+                overridePendingTransition(0,0);
+
+            }
+        });
+
     }
 
 
@@ -78,14 +191,16 @@ public class ProfileActivity extends AppCompatActivity{
                     info.add(temp);
                 }
 
-                TextView user_name = (TextView) findViewById(R.id.display_name);
-                TextView user_major = (TextView) findViewById(R.id.display_major);
-                TextView user_phone = (TextView) findViewById(R.id.display_phone);
-                TextView user_course = (TextView) findViewById(R.id.display_course);
-                TextView user_status = (TextView) findViewById(R.id.display_status);
-                TextView user_email = (TextView) findViewById(R.id.display_email);
+                EditText user_firstname = (EditText) findViewById(R.id.display_name);
+                EditText user_lastname = (EditText) findViewById(R.id.display_lastname);
+                EditText user_major = (EditText) findViewById(R.id.display_major);
+                EditText user_phone = (EditText) findViewById(R.id.display_phone);
+                EditText user_course = (EditText) findViewById(R.id.display_course);
+                EditText user_status = (EditText) findViewById(R.id.display_status);
+                EditText user_email = (EditText) findViewById(R.id.display_email);
 
-                user_name.setText(info.get(2) + " " + info.get(3));
+                user_firstname.setText(info.get(2));
+                user_lastname.setText(info.get(3));
                 user_major.setText(info.get(4));
                 user_phone.setText(info.get(5));
                 user_email.setText(info.get(1));
@@ -102,6 +217,6 @@ public class ProfileActivity extends AppCompatActivity{
             public void onCancelled(DatabaseError databaseError) {
             }
         };
-        mDatabaseRef.addValueEventListener(postListener);
+        mDatabaseRef2.addValueEventListener(postListener);
     }
 }
